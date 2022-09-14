72de180f8da4c3a48c67c4a1bb6013bd1b082589
DESKTOP-S3V5KMT\217g0 <sashimi.travail@gmail.com>
2022-09-14 15:26:37 +0900

    index.html, style.css, main.js スマホ画面時にスムーススクロールバーの追加

---
+++ js/main.js
@@ -0,0 +1,12 @@
+$(function () {
+    $("#page-top").click(function () {
+      $("body, html").animate(
+        {
+          scrollTop: 0,
+        },
+        500
+      );
+      return false;
+    });
+  });
+  
